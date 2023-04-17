49225483b9fe70fc084d876b4f9f2a9c27a15783
conf8o <41929738+conf8o@users.noreply.github.com>
2023-04-17 23:34:36 +0900

    migration command

---
+++ app/migration.py
@@ -0,0 +1,27 @@
+from config import DATABASE_URL
+import sys
+import subprocess
+import os
+
+# 設定ファイルの初期化
+ini_template = open("./migration/alembic.ini_temp.txt").read()
+with open("alembic.ini", "w") as f:
+    f.write(ini_template.format(DATABASE_URL))
+
+# 引数
+_, direction, version = sys.argv
+
+# マイグレーションのフォルダで、alembicを実行
+os.chdir("./migration")
+
+result = subprocess.run(
+    f"alembic {direction} {version}",
+    shell=True,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    text=True
+)
+
+print(result.returncode)
+print(result.stdout)
+print(result.stderr)
