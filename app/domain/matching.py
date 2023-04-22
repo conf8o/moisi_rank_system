11526cddc934b55d6446cae5a1fd45be93b31641
conf8o <41929738+conf8o@users.noreply.github.com>
2023-04-22 18:47:20 +0900

    fix bug for solo matching

--- app/domain/matching.py
+++ app/domain/matching.py
@@ -143,7 +143,7 @@ class SoloMatching:
 
     def make_match(self) -> List[Entry]:
         l = len(self.players)
-        if l < 3:
+        if l <= 3:
             return [Entry(self.players)]
         
         split_l = l // 3
